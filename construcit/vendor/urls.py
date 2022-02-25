0e7447c6686323d09645c6f86d8c51d3429ab027
gkdogar <gk.dogar@gmail.com>
2022-02-25 16:33:32 +0500

    LOGIN DECORATOR ADDED

--- construcit/vendor/urls.py
+++ construcit/vendor/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('vendor/setup', vendor_Setup, name='vendor_setup'),
     path('vendor/setup/<int:pk>/', vendor_Update, name='vendor_update'),
     path('vendor/delete/<int:pk>/', vendor_Delete, name='vendor_delete'),
+    path('login', admin_login, name='admin_login'),
+    path('logout', admin_logout, name='admin_logout'),
+    path('settings', admin_settings, name='admin_setting'),
 ]
--- construcit/vendor/views.py
+++ construcit/vendor/views.py
@@ -14,9 +14,11 @@ from django.contrib.auth.decorators import login_required
 def homePage(request):
     return render(request, 'vendor/base/home.html')
 
+@login_required(login_url='vendor:admin_login')
 def admin_dashboard(request):
     return render(request, 'vendor/dashboard.html') 
 
+@login_required(login_url='vendor:admin_login')
 def vendor_List(request):
     vendor_list = Vendor.objects.all()
     context = {
@@ -25,7 +27,7 @@ def vendor_List(request):
     return render(request, 'vendor/list.html', context)
 
 
-# @login_required(login_url='shopkeeper:admin_login')
+@login_required(login_url='vendor:admin_login')
 def vendor_Setup(request):
     if request.POST:
         print('req', request.FILES)
@@ -62,6 +64,7 @@ def vendor_Setup(request):
             
         return render(request, 'vendor/setup.html')
 
+@login_required(login_url='vendor:admin_login')
 def vendor_Update(request, pk):
     try:
         vendor = Vendor.objects.get(id=pk)
@@ -76,7 +79,7 @@ def vendor_Update(request, pk):
         messages.add_message(request, messages.ERROR, 'Vendor Does Not Exist')
         return redirect('vendor:vendor_setup')
 
-
+@login_required(login_url='vendor:admin_login')
 def vendor_Delete(request, pk):
     try:
         vendor = Vendor.objects.get(id=pk)
@@ -87,3 +90,60 @@ def vendor_Delete(request, pk):
 
         messages.add_message(request, messages.ERROR, 'Record Does not exist for this user')
         return redirect('vendor:vendor_list')
+
+
+# @login_required(login_url='shopkeeper:admin_login')
+@login_required(login_url='vendor:admin_login')
+def admin_settings(request):
+    if request.POST:
+        user = User.objects.get(id=request.user.id)
+       
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.email = request.POST.get('email')
+        user.city = request.POST.get('city')
+        user.phone_no = request.POST.get('phone_no')
+        user.address = request.POST.get('address')
+        user.save()
+        messages.add_message(request, messages.SUCCESS, 'Record Updated Successfully')
+        return redirect('vendor:admin_setting')
+    else:
+        user = User.objects.get(id=request.user.id)
+        context = {
+            'user': user
+        }
+        return render(request, 'vendor/settings.html', context)
+
+
+def admin_login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        email_user = authenticate(email=email, password=password)
+        if email_user:
+            check_user = User.objects.filter(email=email_user)
+            if (email is None) or (password is None):
+                messages.error(request, "Email or Password not given")
+                return redirect('vendor:admin_login')
+            elif (password is None) and (email is None):
+                messages.error(request, "Credentials can't be empty")
+                return redirect('vendor:admin_login')
+            else:
+                if email_user.user_type == 'SUPER_ADMIN':
+                    login(request, email_user)
+                    
+                    return redirect('vendor:admin_dashboard')
+
+                messages.error(request, "You are Not Admin ! Sorry You Can't Login ")
+                return redirect('vendor:admin_login')
+
+        else:
+            messages.add_message(request, messages.ERROR, 'Email or Password not Given')
+            return redirect('vendor:admin_login')
+    else:
+        return render(request, 'vendor/login.html')
+
+@login_required(login_url='vendor:admin_login')
+def admin_logout(request):
+    logout(request)
+    return redirect('vendor:admin_login')
